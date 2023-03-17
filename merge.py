1e651baf19149189e888f845d5e71a2b6cb339d9
Julie Jeanpierre <82562649+juliejeanpierre@users.noreply.github.com>
2023-03-16 22:14:22 -0700

    Create merge.py

---
+++ merge.py
@@ -0,0 +1,19 @@
+def merge(int a, int b):
+  a = []
+  d = []
+  str1 = int(input("enter string length: "))
+  for i in range (1, str1+1):
+    b = int(input("enter number:"))
+    
+    a.append(b)
+    
+    str2 = int(input("enter string length: "))
+  for i in range (1, str2+1):
+    c= int(input("enter number: "))
+    
+    d.append(c)
+    
+    
+   str3 = str1+str2
+   str3.sort()
+    
